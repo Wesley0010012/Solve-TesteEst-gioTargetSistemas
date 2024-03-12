e08937b948c5c94ef646cf3a9a92dec08bd3fe0b
Wesley0010012 <wglaurindo33@gmail.com>
2024-03-11 22:00:07 -0300

    feat: create Teste1 class for exercicise 1

---
+++ classes/teste1.py
@@ -0,0 +1,12 @@
+
+
+class Teste1:
+    def executar(self, indice):
+        k = 0
+        soma = 0
+
+        while k < indice:
+            k = k + 1
+            soma = soma + k
+
+        return soma
