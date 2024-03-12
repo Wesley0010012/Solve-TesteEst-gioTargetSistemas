a97b29f921a9fe6f22e379ba485381af0c9bb113
Wesley0010012 <wglaurindo33@gmail.com>
2024-03-11 22:33:15 -0300

    feat: integrate Teste1 to main

---
+++ main.py
@@ -0,0 +1,11 @@
+from classes.teste1 import Teste1
+from classes.teste2 import Teste2
+from classes.teste3 import Teste3
+from classes.teste5 import Teste5
+
+teste1 = Teste1()
+teste2 = Teste2()
+teste3 = Teste3()
+teste5 = Teste5()
+
+print(f"Resposta do teste 1: A soma para o indice {13} é: {teste1.executar(13)}.")
